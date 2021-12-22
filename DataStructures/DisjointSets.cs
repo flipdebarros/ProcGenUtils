b547c1e52733da1db53ed52b0241dd85c538920c
felipedebarros <felipedebarros@users.noreply.github.com>
2021-12-21 21:28:14 -0300

    Folder Renaming

--- DataStructures/DisjointSets.cs
+++ DataStructures/DisjointSets.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 
-namespace Utils.ProcGenUtils.GraphModel {
+namespace Utils.ProcGenUtils.DataStructures {
 
 /// <summary>
 /// Implements the disjoint sets data structure.
--- GraphModel/GraphAlgorithms.cs
+++ GraphModel/GraphAlgorithms.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Object = UnityEngine.Object;
+using Utils.ProcGenUtils.DataStructures;
 
 namespace Utils.ProcGenUtils.GraphModel {
 
